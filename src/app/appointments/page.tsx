f1ddab98fe83cd6f0cbf008cce8dfe04f8743161
hemuk4631 <hemuk4631@gmail.com>
2025-08-11 17:56:06 +0530

    unused id

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -5,6 +5,7 @@ import Table from '../components/Table';
 function Appointments() {
   const [appointments, setAppointments] = useState([]);
   const handleEdit = useCallback((id: string) => {
+    console.log(id);
   },[]);
 
   const handleDelete = useCallback(async (id: string) => {
