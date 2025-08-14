530af59d5d024873f3aeea746a0056d2a087842c
hemuk4631 <hemuk4631@gmail.com>
2025-08-15 01:44:36 +0530

    db connection

--- src/middleware.ts
+++ src/middleware.ts
@@ -49,6 +49,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api/auth|api/.*|_next/static|_next/image|favicon.ico|signUp|login).*)',
+    '/((?!api/auth|api/.*|_next/static|_next/image|signUp|login).*)',
   ],
 };
